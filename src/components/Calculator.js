75934c1d999ec9f89406fd6b95c42e43c0028033
salek94 <sale.kostic94@gmail.com>
2023-05-01 21:27:42 +0200

    theme

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,99 +1,246 @@
-import React, {useState} from 'react';
-import './calculator.css';
-
+import React, { useState } from "react";
+import "./calculator.css";
 
 const Calculator = () => {
+  const [input, setInput] = useState("");
+  const [prevInput, setPrevInput] = useState("");
+  const [theme, setTheme] = useState("dark");
 
+  const ops = ["+", "-", "*", "/", "."];
 
-  const [input, setInput] = useState('');
-  const [prevInput, setPrevInput] = useState('');
-  const [theme, setTheme] = useState('dark');
-  
-  
-  const ops = ['+','-','*','/','.']
-  
-  function handleClick(value){
-    
+  function handleClick(value) {
     if (
-      (ops.includes(value) && input === '') ||
+      (ops.includes(value) && input === "") ||
       (ops.includes(value) && ops.includes(input.slice(-1)))
-      ) return;
-  
+    )
+      return;
+
     setInput(input + value);
-    setPrevInput(eval(input + value).toString())
+    setPrevInput(eval(input + value).toString());
     // setPrevInput(Function('return' + input + value)());
   }
 
-  function handleDel(){
-    
-    if (input === '') return;
-  
-    const delValue = input.slice(0,-1);
-    
+  function handleDel() {
+    if (input === "") return;
+
+    const delValue = input.slice(0, -1);
+
     setInput(delValue);
-    setPrevInput(eval(delValue))
-    
-    if (delValue === '') setPrevInput('');
+    setPrevInput(eval(delValue));
+
+    if (delValue === "") setPrevInput("");
   }
 
   function handleClear() {
-    setInput('');
-    setPrevInput('');
+    setInput("");
+    setPrevInput("");
   }
 
   function handleEqual() {
-    if (input === '') return;
+    if (input === "") return;
 
     setInput(eval(input).toString());
-    setPrevInput('');
+    setPrevInput("");
   }
 
   function handleTheme() {
-    setTheme((prev)=>prev === 'light' ? 'dark' : 'light')
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
   }
+
+  setInterval(() => {
+    let random = Math.floor(Math.random() * 5);
+    console.log(random);
+  }, 1000000);
+  clearInterval();
 
   return (
     <div>
       <h1>Calculator</h1>
       <div className={`container ${theme}`}>
-          <div className='output'>
-            <div>{prevInput}</div>
-            <div>{input}</div>
-          </div>
-          <div className='wrapper'>
-              <button type="button" disabled={input === ""} onClick={handleClear} className='gray'>C</button>
-              <button type="button" onClick={()=>{handleClick('(')}} className='gray'>(</button>
-              <button type="button" onClick={()=>{handleClick(')')}} className='gray'>)</button>
-              <button type="button" disabled={input === ""} onClick={handleDel} className={`${theme === 'dark'? 'orange' : 'gray'}`} >DEL</button>  
-          </div>
-          <div className='wrapper'>
-              <button type="button" onClick={()=>{handleClick('7')}} className={`${theme === 'light'?'blue':''}`} >7</button>
-              <button type="button" onClick={()=>{handleClick('8')}}>8</button>
-              <button type="button" onClick={()=>{handleClick('9')}}>9</button>
-              <button type="button" onClick={()=>{handleClick('*')}} className={`${theme === 'dark'? 'orange' : 'gray'}`} >x</button>
-          </div>
-          <div className='wrapper'>
-              <button type="button" onClick={()=>{handleClick('4')}}>4</button>
-              <button type="button" onClick={()=>{handleClick('5')}}>5</button>
-              <button type="button" onClick={()=>{handleClick('6')}}>6</button>
-              <button type="button" onClick={()=>{handleClick('-')}} className={`${theme === 'dark'? 'orange' : 'gray'}`} >-</button>
-          </div>
-          <div className='wrapper'>
-              <button type="button" onClick={()=>{handleClick('1')}}>1</button>
-              <button type="button" onClick={()=>{handleClick('2')}}>2</button>
-              <button type="button" onClick={()=>{handleClick('3')}}>3</button>
-              <button type="button" onClick={()=>{handleClick('+')}} className={`${theme === 'dark'? 'orange' : 'gray'}`} >+</button>
-          </div>
-          <div className='wrapper'>
-              <button type="button" onClick={()=>{handleClick('0')}}>0</button>
-              <button type="button" onClick={()=>{handleClick('.')}}>.</button>
-              <button type="button" onClick={()=>{handleClick('/')}}>/</button>
-              <button type="button" onClick={handleEqual} className={`${theme === 'dark'? 'orange' : 'gray'}`}>=</button>
-          </div>
+        <div className="output">
+          <div>{prevInput}</div>
+          <div>{input}</div>
+        </div>
+        <div className="wrapper">
+          <button
+            type="button"
+            disabled={input === ""}
+            onClick={handleClear}
+            className="gray"
+          >
+            C
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("(");
+            }}
+            className="gray"
+          >
+            (
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick(")");
+            }}
+            className="gray"
+          >
+            )
+          </button>
+          <button
+            type="button"
+            disabled={input === ""}
+            onClick={handleDel}
+            className={`${theme === "dark" ? "orange" : "gray"}`}
+          >
+            DEL
+          </button>
+        </div>
+        <div className="wrapper">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("7");
+            }}
+          >
+            7
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("8");
+            }}
+          >
+            8
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("9");
+            }}
+          >
+            9
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("*");
+            }}
+            className={`${theme === "dark" ? "orange" : "gray"}`}
+          >
+            x
+          </button>
+        </div>
+        <div className="wrapper">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("4");
+            }}
+          >
+            4
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("5");
+            }}
+          >
+            5
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("6");
+            }}
+          >
+            6
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("-");
+            }}
+            className={`${theme === "dark" ? "orange" : "gray"}`}
+          >
+            -
+          </button>
+        </div>
+        <div className="wrapper">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("1");
+            }}
+          >
+            1
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("2");
+            }}
+          >
+            2
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("3");
+            }}
+          >
+            3
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("+");
+            }}
+            className={`${theme === "dark" ? "orange" : "gray"}`}
+          >
+            +
+          </button>
+        </div>
+        <div className="wrapper">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("0");
+            }}
+          >
+            0
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick(".");
+            }}
+          >
+            .
+          </button>
+          <button
+            type="button"
+            onClick={() => {
+              handleClick("/");
+            }}
+          >
+            /
+          </button>
+          <button
+            type="button"
+            onClick={handleEqual}
+            className={`${theme === "dark" ? "orange" : "gray"}`}
+          >
+            =
+          </button>
+        </div>
       </div>
-      <button onClick={handleTheme}>theme</button>
+      <button className="themeBtn" onClick={handleTheme}>
+        theme
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Calculator
+export default Calculator;
